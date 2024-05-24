b5335ec7e2b21d4ada9ff67ba906b26365dc6db3
Matias Kamelman <125071787+kamelmat@users.noreply.github.com>
2024-05-24 17:09:19 +0000

    Style according PEP8

--- src/app.py
+++ src/app.py
@@ -8,34 +8,29 @@ from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
 #from models import Person
 
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
+jackson_family = FamilyStructure("Jackson") # create the jackson family object
 
-# create the jackson family object
-jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
+@app.errorhandler(APIException) # Handle/serialize errors like a JSON object
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
-@app.route('/')
+
+@app.route('/') # generate sitemap with all your endpoints
 def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
+    response_body = {}
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body["hello"] = "world"
+    response_body["family"] = members             
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
